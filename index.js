ca30319d006a6ff9d6f185589559ddaa0038f795
Patrik Hartwig <pat.ggf@gmail.com>
2016-12-21 21:24:26 +1000

    up

--- index.js
+++ index.js
@@ -9,13 +9,23 @@ board.on('ready', function () {
   // Create an LED on the XIO-P0 pin
   var led = new five.Led('XIO-P0');
   led.blink(500);
-  console.log(':|');
+
+
+  var led2 = new five.Led('PWM0');
+  // Make it pulse with a 1 second interval
+  led2.pulse(1000);
+
+  var thermometer = new chipio.InternalTemperature();
+  thermometer.on('change', function (data) {
+    console.log('Internal temperature is ' + data.celsius.toFixed(2) + '°C');
+  });
+
+  // create battery voltage sensor
+  var batteryVoltage = new chipio.BatteryVoltage();
+
+  // listen for value changes
+  batteryVoltage.on('change', function (voltage) {
+    console.log('Battery voltage is ' + voltage.toFixed(2) + 'V');
+  });
+
 });
-
-board.hello = function () {
-  console.log('123');
-};
-
-function hello() {
-  console.log('432');
-}
