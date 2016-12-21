e08a9f006c16fe77f8b1bc960362e8ac66016dd7
Patrik Hartwig <pat.ggf@gmail.com>
2016-12-21 21:02:49 +1000

    de

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ var board = new five.Board({
   io: new chipio()
 });
 
-board.on('ready', function() {
+board.on('ready', function () {
   // Create an LED on the XIO-P0 pin
   var led = new five.Led('XIO-P0');
-
-  // Blink every half second
   led.blink(500);
+  console.log(':|');
 });
+
+board.hello = function () {
+  console.log('123');
+};
+
+function hello() {
+  console.log('432');
+}
