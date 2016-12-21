3c636d35b176e219ff956ca2352885538e9e3f8d
root <pat.ggf@gmail.com>
2016-12-21 21:01:15 +1000

    up

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var board = new five.Board({
   io: new chipio()
 });
 
-board.on('ready', function () {
-  // do Johnny-Five stuff
+board.on('ready', function() {
+  // Create an LED on the XIO-P0 pin
+  var led = new five.Led('XIO-P0');
+
+  // Blink every half second
+  led.blink(500);
 });
